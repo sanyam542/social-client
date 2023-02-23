fc8f58f0d04f90d5f85836d95330faa4694956fc
Sanyam Singh <sanyamsingh@Sanyams-MacBook-Air.local>
2023-02-23 06:17:21 +0530

    socket2

--- src/pages/messenger/Messenger.jsx
+++ src/pages/messenger/Messenger.jsx
@@ -48,7 +48,10 @@ export default function Messenger() {
   useEffect(() => {
     const getConversations = async () => {
       try {
-        const res = await axios.get("/conversations/" + user._id);
+        const res = await axios.get(
+          "https://thunderous-pasca-e2cda8.netlify.app/conversations/" +
+            user._id
+        );
         setConversations(res.data);
       } catch (err) {
         console.log(err);
@@ -60,7 +63,10 @@ export default function Messenger() {
   useEffect(() => {
     const getMessages = async () => {
       try {
-        const res = await axios.get("/messages/" + currentChat?._id);
+        const res = await axios.get(
+          "https://thunderous-pasca-e2cda8.netlify.app/api/messages/" +
+            currentChat?._id
+        );
         setMessages(res.data);
       } catch (err) {
         console.log(err);
@@ -88,7 +94,10 @@ export default function Messenger() {
     });
 
     try {
-      const res = await axios.post("/messages", message);
+      const res = await axios.post(
+        "https://thunderous-pasca-e2cda8.netlify.app/api/messages",
+        message
+      );
       setMessages([...messages, res.data]);
       setNewMessage("");
     } catch (err) {
